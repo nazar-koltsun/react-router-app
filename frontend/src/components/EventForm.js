43122a6a161a35e331a7f76d3b5e866e47d173c3
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-06-13 13:30:34 +0300

    Used useRouteLoaderData to have access to data from several components

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -4,6 +4,7 @@ import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+
   function cancelHandler() {
     navigate('..');
   }
@@ -12,19 +13,19 @@ function EventForm({ method, event }) {
     <form className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required />
+        <input id="title" defaultValue={event.title} type="text" name="title" required />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required />
+        <input id="image" defaultValue={event.image} type="url" name="image" required />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required />
+        <input id="date" defaultValue={event.date} type="date" name="date" required />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required />
+        <textarea id="description" defaultValue={event.description} date name="description" rows="5" required />
       </p>
       <div className={classes.actions}>
         <button type="button" onClick={cancelHandler}>
